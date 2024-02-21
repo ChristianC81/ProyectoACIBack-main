d49de6b68d0e2fb1a68064bcc9f43b72b1f55b21
CAB3ZON <marcospillcocontreras@gmail.com>
2024-02-21 13:13:29 -0500

    Añadido de filtrado en responsables
    pillco marcos

--- src/main/java/com/sistema/examenes/services/Asignacion_Evidencia_Service.java
+++ src/main/java/com/sistema/examenes/services/Asignacion_Evidencia_Service.java
@@ -15,6 +15,8 @@ public interface Asignacion_Evidencia_Service extends GenericService<Asignacion_
     List<AsignacionEvidenciaProyeccion> listarAsignacionEvidenciaProyeccion();
     Boolean verificarAsignacionUsuario(Long id_usuario, Long id_evidencia,Long id_modelo);
     List<AsignaProjection> listarAsigEvidencia();
+
+    List<AsignaProjection>listarAsigEvidenciaPorUsuario(Long usuarioId);
     Asignacion_Evidencia fechaactividades(Long id_evidencia,Long id_modelo);
 
 
